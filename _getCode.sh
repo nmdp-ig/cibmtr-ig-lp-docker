6e1b1e19f308ea40c1af16a686826e8ead0867fe
Scott Collins <scollins@nmdp.org>
2020-06-19 13:12:49 -0500

    move code retrieval outside of docker build process

--- _getCode.sh
+++ _getCode.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm -rf public_html/*
 rm -rf gatsby-build
 mkdir -p gatsby-build
 git clone https://a7b9ad562d57b74c1526907a61a7f54ab7192536@github.com/nmdp-ig/cibmtr-ig-landing.git gatsby-build/
@@ -10,6 +11,11 @@ cd public
 mkdir -p ig
 cd ig
 git clone -b deploy-ready https://a7b9ad562d57b74c1526907a61a7f54ab7192536@github.com/nmdp-ig/hla-reporting-ig.git hla-reporting/
+cd ..
+cd ..
+cd ..
+cp -rf gatsby-build/public/ public_html/ 
+
 
 
 
