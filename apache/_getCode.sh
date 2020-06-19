4b1e4d8ecfde66d51e59397a3f70ee78feb7f357
Scott Collins <scollins@nmdp.org>
2020-06-19 12:05:16 -0500

    run script to get code in apache container

---
+++ apache/_getCode.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+rm -rf gatsby-build
+mkdir -p gatsby-build
+git clone https://a7b9ad562d57b74c1526907a61a7f54ab7192536@github.com/nmdp-ig/cibmtr-ig-landing.git gatsby-build/
+cd gatsby-build
+npm install -g gatsby
+npm ci
+gatsby build
+cd public
+mkdir -p ig
+cd ig
+git clone -b deploy-ready https://a7b9ad562d57b74c1526907a61a7f54ab7192536@github.com/nmdp-ig/hla-reporting-ig.git hla-reporting/
+
+
+
