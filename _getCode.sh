68deeb1fbeb85f7a839040e76e5c0fbc895fa193
Jason Brelsford <33839713+jbrelsf2-nmdp@users.noreply.github.com>
2020-07-06 16:58:26 -0500

    Update _getCode.sh

--- _getCode.sh
+++ _getCode.sh
@@ -2,7 +2,7 @@
 rm -rf public_html/*
 rm -rf gatsby-build
 mkdir -p gatsby-build
-git clone https://a7b9ad562d57b74c1526907a61a7f54ab7192536@github.com/nmdp-ig/cibmtr-ig-landing.git gatsby-build/
+git clone https://github.com/nmdp-ig/cibmtr-ig-landing.git gatsby-build/
 cd gatsby-build
 npm install -g gatsby
 npm ci
@@ -10,7 +10,7 @@ gatsby build
 cd public
 mkdir -p ig
 cd ig
-git clone -b deploy-ready https://a7b9ad562d57b74c1526907a61a7f54ab7192536@github.com/nmdp-ig/hla-reporting-ig.git hla-reporting/
+git clone -b deploy-ready https://github.com/nmdp-ig/hla-reporting-ig.git hla-reporting/
 cd ..
 cd ..
 cd ..
